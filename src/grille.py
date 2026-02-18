56f21910cb4014551c5a6d9932b2ab4ddce3e610
Noam <2467523@bdeb.qc.ca>
2026-02-18 16:27:44 -0500

    structure initiale de la grille.py

--- src/grille.py
+++ src/grille.py
@@ -1,3 +1,21 @@
+import pyvista as pv
+import numpy as np
+
 class Grille:
-    def __init__(self):
-        pass
+    def __init__(self, x, y, z):
+
+        self.valeurs = {
+            "vx" : np.zeros((x,y,z)),
+            "vy" : np.zeros((x,y,z)),
+            "vz" : np.zeros((x,y,z)),
+            "densitee" : np.ones((x,y,z))
+        }
+
+        self.mesh = pv.ImageData(
+            dimensions=(x,y,z),
+            spacing=(1, 1, 1),
+            origin=(0, 0, 0)
+        )
+
+        for name, arr in self.valeurs.items():
+            self.mesh.cell_data[name] = arr.flatten(order="F")
