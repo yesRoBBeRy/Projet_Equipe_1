f2f4b3a509a82b36089502a5e550f7deb14e1257
moute40 <famajor77@gmail.com>
2026-03-05 11:37:48 -0500

    caca

--- src/main_window.py
+++ src/main_window.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QHBoxLayout, QMainWindow, QVBoxLayout, QWidget
+from PySide6.QtCore import QTimer
+from PySide6.QtWidgets import QHBoxLayout, QMainWindow, QVBoxLayout, QWidget, QPushButton
 
 from src.scene_3D import Scene3D
 from src.grille import Grille
@@ -7,17 +8,45 @@ from src.grille import Grille
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.grille = Grille(9,9,9)
+        self.grille = Grille(5,5,10)
 
         self.resize(800, 600)
 
-        centre = QWidget()
-        self.setCentralWidget(centre)
+        central = QWidget()
+        self.setCentralWidget(central)
 
-        layout = QHBoxLayout(centre)
+        # Main horizontal layout
+        mainLayout = QHBoxLayout(central)
 
-        self.scene = Scene3D(centre, self.grille)
+        # Left and Right panels
+        left = QWidget()
+        right = QWidget()
 
-        layout.addWidget(self.scene.plotter)
+        mainLayout.addWidget(left)
+        mainLayout.addWidget(right)
+
+        layoutLeft = QVBoxLayout(left)
+        layoutRight = QVBoxLayout(right)
+
+        # Scene on the left
+        self.scene = Scene3D(left, self.grille)
+        layoutLeft.addWidget(self.scene.plotter)
+
+        # Button on the right
+        self.button = QPushButton("Start")
+        layoutRight.addWidget(self.button)
 
         self.scene.plotter.show()
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update) # renamed
+        self.timer.start(1000 // 30)
+
+    def update(self):
+        self.grille.update_valeurs()
+        self.scene.update_scene()
+        print("oenis")
+
+
+
+
